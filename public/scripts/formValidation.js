2848761dd8954635bade941ebc0fed5a32bc2032
NhanNguyen <nnguyen14@sfsu.edu>
2023-11-24 18:37:12 -0800

    add js for form validation

---
+++ public/scripts/formValidation.js
@@ -0,0 +1,39 @@
+// formValidation.js
+
+function validateForm(formId) {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("passwd").value;
+  var isValid = true;
+
+  // Reset previous error messages
+  document.getElementById("emailError").textContent = "";
+  document.getElementById("passwordError").textContent = "";
+  var confirmPasswordError = document.getElementById("confirmPasswordError");
+  if (confirmPasswordError) confirmPasswordError.textContent = "";
+
+  // Email validation
+  if (!email.match(/^\S+@\S+\.\S+$/)) {
+    document.getElementById("emailError").textContent =
+      "Please enter a valid email address.";
+    isValid = false;
+  }
+
+  // Password validation
+  if (password.length < 6) {
+    document.getElementById("passwordError").textContent =
+      "Password must be at least 6 characters.";
+    isValid = false;
+  }
+
+  // Confirm Password validation for account creation form
+  if (formId === "accountCreationForm") {
+    var confirmPassword = document.getElementById("re-passwd").value;
+    if (password !== confirmPassword) {
+      if (confirmPasswordError)
+        confirmPasswordError.textContent = "Passwords do not match.";
+      isValid = false;
+    }
+  }
+
+  return isValid;
+}
